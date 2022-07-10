82a44a3c7845a97466f7518344dfa030b370f9e0
RAJAT SINGH <106505102+rajatrexon@users.noreply.github.com>
2022-07-10 21:50:45 +0530

    Update DenominationService.java

--- TravellerProject/src/com/greatlearning/currency/DenominationService.java
+++ TravellerProject/src/com/greatlearning/currency/DenominationService.java
@@ -3,6 +3,11 @@ package com.greatlearning.currency;
 public class DenominationService {
 	public void notesCalculator(int[] denominations, int payingAmount) {
 	try {
+		if(payingAmount<denominations[denominations.length-1])
+		{
+			System.out.println("Amount can't be paid by available denominations");
+			return;
+		}
 		
 		for(int i = 0; i<denominations.length;i++) {
 			if(payingAmount>=denominations[i]) {
